83d3573d765799fda8c40b34906fbedb43630ac2
Yamil1303 <u795890455@byui.edu>
2020-10-10 14:42:22 -0500

    master

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -2,8 +2,12 @@ function toggle() {
     console.log(document.getElementById("primaryNav").classList.toggle("hide"));
 }
 
-var modified = document.lastModified;
-document.getElementById("last_updated").innerHTML=modified;
-
 var year = new Date().getFullYear();
 document.getElementById("year").innerHTML=year;
+
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fiday", "Saturday"]
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var d = new Date();
+    var date_now = days[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+    document.getElementById("date").innerHTML= date_now;
