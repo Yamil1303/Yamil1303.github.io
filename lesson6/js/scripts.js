37877ea946b2d1aa653cd81182db930668e07a23
Yamil1303 <u795890455@byui.edu>
2020-10-24 00:10:46 -0500

    master

---
+++ lesson6/js/scripts.js
@@ -0,0 +1,17 @@
+function toggle() {
+    console.log(document.getElementById("primaryNav").classList.toggle("hide"));
+    /*var toggleclass = document.getElementById("primaryNav").classList;
+    if (toggleclass != "hide") {
+        document.getElementById("summary").style.top="380px";
+    } else {document.getElementById("summary").style.top="140px";}*/
+}
+
+var year = new Date().getFullYear();
+document.getElementById("year").innerHTML=year;
+
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fiday", "Saturday"]
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var d = new Date();
+    var date_now = days[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+    document.getElementById("date").innerHTML= date_now;
