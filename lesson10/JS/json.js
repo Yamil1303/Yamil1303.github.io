c8421b14ae6a08d293c66a2672bf5a845df72890
Yamil1303 <u795890455@byui.edu>
2020-11-20 16:04:18 -0500

    master

---
+++ lesson10/JS/json.js
@@ -0,0 +1,38 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++ ) {
+            let card = document.createElement('section');
+            let div = document.createElement('div');
+            let name = document.createElement('h2');
+            let motto = document.createElement('h3');
+            let year = document.createElement('p');
+            let population = document.createElement('p');
+            let rainfall = document.createElement('p');
+            let img = document.createElement('img');
+
+            name.textContent = towns[i].name;
+            motto.textContent = towns[i].motto;
+            year.textContent = "Year Founded: " + towns[i].yearFounded; 
+            population.textContent = "Population: " + towns[i].currentPopulation;
+            rainfall.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
+            img.setAttribute('src', "images/" + towns[i].photo);
+            img.setAttribute('alt', "Photograph of: " + name.textContent);
+
+            if (towns[i].name=="Fish Haven" || towns[i].name== "Preston" || towns[i].name=="Soda Springs"){
+            div.appendChild(name);
+            div.appendChild(motto);
+            div.appendChild(year);
+            div.appendChild(population);
+            div.appendChild(rainfall);
+            card.appendChild(div);
+            card.appendChild(img);
+
+            document.querySelector('div.cities').appendChild(card);}
+    }
+  });
---
+++ lesson10/JS/weather.js
@@ -0,0 +1,12 @@
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=57eab0ca980a33623115672515a2c752&units=imperial";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    console.log(weatherInfo);
+
+    document.getElementById(cur).innerHTML=weatherInfo.weather[0].main;
+    document.getElementById(hum).innerHTML=weatherInfo.main.humidity;
+    document.getElementById(temp).innerHTML=weatherInfo.main.temp;
+    document.getElementById(speed).innerHTML=weatherInfo.wind.speed;
+  });
