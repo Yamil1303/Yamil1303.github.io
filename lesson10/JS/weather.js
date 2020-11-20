826be45c72bd76473f3bc6203e332f11bb41029f
Yamil1303 <u795890455@byui.edu>
2020-11-20 16:45:38 -0500

    MASTER

--- lesson10/JS/weather.js
+++ lesson10/JS/weather.js
@@ -3,10 +3,20 @@ const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=57eab
 fetch(apiURL)
   .then((response) => response.json())
   .then((weatherInfo) => {
-    console.log(weatherInfo);
+    //console.log(weatherInfo);
 
-    document.getElementById(cur).innerHTML=weatherInfo.weather[0].main;
-    document.getElementById(hum).innerHTML=weatherInfo.main.humidity;
-    document.getElementById(temp).innerHTML=weatherInfo.main.temp;
-    document.getElementById(speed).innerHTML=weatherInfo.wind.speed;
+    document.getElementById("cur").innerHTML=weatherInfo.weather[0].main;
+    document.getElementById("hum").innerHTML=weatherInfo.main.humidity;
+    var tempNum = document.getElementById("temp").innerHTML=weatherInfo.main.temp;
+    var speedNumber = document.getElementById("speed").innerHTML=weatherInfo.wind.speed;
+
+    /* WIND CHILL CALCULATION */
+    let windchill = 35.74 + 0.6215 * tempNum - 35.75 * Math.pow(speedNumber, 0.16) + 0.4275 * tempNum * Math.pow(speedNumber, 0.16);
+    windchill = Math.round(windchill);
+    if (tempNum <=50 && speedNumber > 3) {
+        document.getElementById("chill").textContent="Wind Chill: " + windchill + "\xB0F";
+    } else{
+        document.getElementById("chill").textContent="No Wind Chill today";
+    }
+
   });
--- lesson10/JS/windchill.js
+++ lesson10/JS/windchill.js
@@ -4,7 +4,7 @@ let windchill = 35.74 + 0.6215 * tempNum - 35.75 * Math.pow(speedNumber, 0.16) +
 windchill = Math.round(windchill);
 
 if (tempNum <=50 && speedNumber > 3) {
-    document.getElementById("chill").textContent="Wind Chill: " + windchill + "\xB0F"
+    document.getElementById("chill").textContent="Wind Chill: " + windchill + "\xB0F";
 } else{
-    document.getElementById("chill").textContent="No Wind Chill today"
+    document.getElementById("chill").textContent="No Wind Chill today";
 }
