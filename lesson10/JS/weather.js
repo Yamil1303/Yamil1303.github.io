125b187671be499bca9da459b2cca819285ea7bf
Yamil1303 <u795890455@byui.edu>
2020-11-20 23:22:20 -0500

    master

--- lesson10/JS/weather.js
+++ lesson10/JS/weather.js
@@ -1,10 +1,14 @@
 const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=57eab0ca980a33623115672515a2c752&units=imperial";
 
+
+
+/* ----------------Fetch -----------------*/
 fetch(apiURL)
   .then((response) => response.json())
   .then((weatherInfo) => {
     //console.log(weatherInfo);
 
+    /* ------------------ Weather Summary -------------------*/
     document.getElementById("cur").innerHTML=weatherInfo.weather[0].main;
     document.getElementById("hum").innerHTML=weatherInfo.main.humidity;
     var tempNum = document.getElementById("temp").innerHTML=weatherInfo.main.temp;
@@ -20,3 +24,4 @@ fetch(apiURL)
     }
 
   });
+
