650d6adaf090c2fc5a6242023c81d72f8b54fd06
Yamil1303 <u795890455@byui.edu>
2020-11-23 18:29:29 -0500

    master

---
+++ lesson11/JS/weather.js
@@ -0,0 +1,28 @@
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=57eab0ca980a33623115672515a2c752&units=imperial";
+
+
+
+/* ----------------Fetch -----------------*/
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //console.log(weatherInfo);
+
+    /* ------------------ Weather Summary -------------------*/
+    document.getElementById("title").innerHTML=weatherInfo.name;
+    document.getElementById("cur").innerHTML=weatherInfo.weather[0].main;
+    document.getElementById("hum").innerHTML=weatherInfo.main.humidity;
+    var tempNum = document.getElementById("temp").innerHTML=weatherInfo.main.temp;
+    var speedNumber = document.getElementById("speed").innerHTML=weatherInfo.wind.speed;
+
+    /* WIND CHILL CALCULATION */
+    let windchill = 35.74 + 0.6215 * tempNum - 35.75 * Math.pow(speedNumber, 0.16) + 0.4275 * tempNum * Math.pow(speedNumber, 0.16);
+    windchill = Math.round(windchill);
+    if (tempNum <=50 && speedNumber > 3) {
+        document.getElementById("chill").textContent="Wind Chill: " + windchill + "\xB0F";
+    } else{
+        document.getElementById("chill").textContent="No Wind Chill today";
+    }
+
+  });
+
