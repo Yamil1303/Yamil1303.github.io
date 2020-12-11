1dc9c325c812417501e52fb373911c3534a694f7
Yamil1303 <u795890455@byui.edu>
2020-12-10 22:36:43 -0500

    master

--- termProject/js/json.js
+++ termProject/js/json.js
@@ -6,28 +6,34 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         const rentals = jsonObject['rentals'];
-        console.log(rentals);
+        console.log(rentals)
         for (let i = 0; i < rentals.length; i++ ) {
             let card = document.createElement('section');
             let div = document.createElement('div');
             let name = document.createElement('h2');
             let max = document.createElement('p');
-            let population = document.createElement('p');
-            let rainfall = document.createElement('p');
+            let rhalf = document.createElement('p');
+            let rfull = document.createElement('p');
+            let whalf = document.createElement('p');
+            let wfull = document.createElement('p');
             let img = document.createElement('img');
 
-            max.textContent = rentals[i].max. persons;
-            year.textContent = "Year Founded: " + rentals[i].yearFounded; 
-            population.textContent = "Population: " + rentals[i].currentPopulation;
-            rainfall.textContent = "Annual Rain Fall: " + rentals[i].averageRainfall;
+            name.textContent = rentals[i].name;
+            max.textContent = "Max. persons: " + rentals[i].maxPersons; 
+            rhalf.textContent = "Half day reservation: " + rentals[i].reservation["half day"] + "$";
+            rfull.textContent = "Full day reservation: " + rentals[i].reservation["full day"] + "$";
+            whalf.textContent = "Half day walk-in: " + rentals[i]["walk-in"]["half day"] + "$";
+            wfull.textContent = "Full day walk-in: " + rentals[i]["walk-in"]["full day"] + "$";
             img.setAttribute('id', "img");  //edited
             img.setAttribute('src', "images/" + rentals[i].photo);
             img.setAttribute('alt', "Photograph of: " + name.textContent);
 
             div.appendChild(name);
             div.appendChild(max);
-            div.appendChild(population);
-            div.appendChild(rainfall);
+            div.appendChild(rhalf);
+            div.appendChild(rfull);
+            div.appendChild(whalf);
+            div.appendChild(wfull);
             card.appendChild(div);
             card.appendChild(img);
 
