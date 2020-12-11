3b90607a0734bc17e9a6abc532cd94eaae37798d
Yamil1303 <u795890455@byui.edu>
2020-12-10 21:46:17 -0500

    master

--- termProject/js/json.js
+++ termProject/js/json.js
@@ -1,39 +1,36 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = 'https://yamil1303.github.io/termProject/json/chart.json';
 
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-        const towns = jsonObject['rentals'];
-        for (let i = 0; i < towns.length; i++ ) {
+        const rentals = jsonObject['rentals'];
+        console.log(rentals);
+        for (let i = 0; i < rentals.length; i++ ) {
             let card = document.createElement('section');
             let div = document.createElement('div');
             let name = document.createElement('h2');
-            let motto = document.createElement('h3');
-            let year = document.createElement('p');
+            let max = document.createElement('p');
             let population = document.createElement('p');
             let rainfall = document.createElement('p');
             let img = document.createElement('img');
 
-            if (towns[i].name=="Fish Haven" || towns[i].name== "Preston" || towns[i].name=="Soda Springs"){
-            name.textContent = towns[i].name;
-            motto.textContent = towns[i].motto;
-            year.textContent = "Year Founded: " + towns[i].yearFounded; 
-            population.textContent = "Population: " + towns[i].currentPopulation;
-            rainfall.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
+            max.textContent = rentals[i].max. persons;
+            year.textContent = "Year Founded: " + rentals[i].yearFounded; 
+            population.textContent = "Population: " + rentals[i].currentPopulation;
+            rainfall.textContent = "Annual Rain Fall: " + rentals[i].averageRainfall;
             img.setAttribute('id', "img");  //edited
-            img.setAttribute('src', "images/" + towns[i].photo);
+            img.setAttribute('src', "images/" + rentals[i].photo);
             img.setAttribute('alt', "Photograph of: " + name.textContent);
 
             div.appendChild(name);
-            div.appendChild(motto);
-            div.appendChild(year);
+            div.appendChild(max);
             div.appendChild(population);
             div.appendChild(rainfall);
             card.appendChild(div);
             card.appendChild(img);
 
-            document.querySelector('div.cities').appendChild(card);}
+            document.querySelector('div.rentals').appendChild(card);
     }
   });
